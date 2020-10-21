8513a0ef613a187a7b0adc4150f1dd328796b9e1
Ivan Savkov <ivansavkov1387@gmail.com>
2020-10-21 12:21:48 +0300

    1.Create An App

--- ColorizedAppSwiftUI/ContentView.swift
+++ ColorizedAppSwiftUI/ContentView.swift
@@ -9,13 +9,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var sliderValueR : Double = 0
+    @State private var sliderValueG : Double = 0
+    @State private var sliderValueB : Double = 0
+    
     var body: some View {
-        Text("Hello, World!")
+        ZStack {
+            Color.init(.lightGray)
+                .edgesIgnoringSafeArea(.all)
+            VStack (spacing: 20) {
+                Color.init(red: (sliderValueR / 255) ,
+                           green: (sliderValueG / 255) ,
+                           blue: (sliderValueB / 255))
+                    .cornerRadius(15)
+                    .frame(height: 200.0)
+                    .overlay(RoundedRectangle.init(cornerRadius: 20)
+                        .stroke(lineWidth: 4) .foregroundColor(.white))
+                
+                ColorSlider(value: $sliderValueR).accentColor(.red)
+                ColorSlider(value: $sliderValueG).accentColor(.green)
+                ColorSlider(value: $sliderValueB).accentColor(.blue)
+                Spacer()
+            }.padding()
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct ColorSlider: View {
+    @Binding var value: Double
+    let formatter = NumberFormatter()
+    
+    var body: some View {
+        HStack() {
+            Text(String(lround(value))).foregroundColor(.black)
+            Slider(value: $value, in: 0...255, step: 1)
+            TextField(String(lround(value)), value: $value, formatter: formatter)
+                .frame(width: 40)
+                .background(Color.init(.white)
+                .cornerRadius(7))
+        }
     }
 }
