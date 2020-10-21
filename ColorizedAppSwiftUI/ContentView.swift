7b73cbcfd959073a5f8fdc873c5fd5f3dbf9d1f4
Ivan Savkov <ivansavkov1387@gmail.com>
2020-10-21 10:15:07 +0300

    Initial Commit

---
+++ ColorizedAppSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ColorizedAppSwiftUI
+//
+//  Created by Иван on 10/21/20.
+//  Copyright © 2020 Ivan Savkov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
